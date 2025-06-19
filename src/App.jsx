f5a0db8c405ce03d392f5150d39135c4dbfb0142
Timo <timo23075@gmail.com>
2025-06-19 20:17:16 +0200

    first commit and build

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </>
+  )
+}
+
+export default App

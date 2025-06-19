2f5ed60ed76fce14ee711f87bcf5bfa09649a664
Timo <timo23075@gmail.com>
2025-06-19 20:59:57 +0200

    add languages list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { languages } from "./languages"
 
 function App() {
+  
 
   return (
     <>
@@ -8,9 +10,12 @@ function App() {
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
       <div className="status">
-        <h1>You Win!</h1>
-        <h2>Well done</h2>
+        <h2>You Win!</h2>
+        <p>Well done 🎉</p>
       </div>
+      <section className="languages">
+          {languages.map(el => <p style={{color: el.color, backgroundColor: el.backgroundColor}} key={el.name}>{el.name}</p>)}
+      </section>
     </>
   )
 }

74b2d3740a4cf0796406da1fee79529489d3e0a4
Timo <timo23075@gmail.com>
2025-06-19 20:33:19 +0200

    add header and win/lose section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+      <div className="status">
+        <h1>You Win!</h1>
+        <h2>Well done</h2>
+      </div>
     </>
   )
 }

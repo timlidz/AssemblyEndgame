cfc06d4c5cc2651233b2a76d75208fda2633391f
Timo <timo23075@gmail.com>
2025-06-21 22:09:22 +0200

    adding clsx and wrong and correct button styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { languages } from "./languages"
 import React from "react"
+import { clsx } from 'clsx';
 
 
 function App() {
   const [word, setWord] = React.useState("react")
+  const [letters, setLetters] = React.useState([])
 
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+  function guessLetters(letter){
+    setLetters(prev => prev.includes(letter) ? prev : 
+    [...prev, letter] )
+  }
+
+  function buttonColors(letter) {
+    const classVar = [...word].includes(letter) ? 'key-correct' : 'key-wrong'
+    return classVar
+  }
 
   return (
     <>
@@ -25,8 +36,18 @@ function App() {
         {word.split('').map((el, index) => <span key={index}>{el.toUpperCase()}</span>)}
       </section>
       <section className="keyboard">
-        {[...alphabet].map(el => <button key={el}>{el.toUpperCase()}</button>)}
+        {[...alphabet].map(el => {
+          const isGuessed = letters.includes(el)
+          const isCorrect = isGuessed && word.includes(el)
+          const isWrong = isGuessed && !word.includes(el)
+          const className = clsx({'key-correct':isCorrect, 'key-wrong':isWrong})
+          return (
+            <button className={className}
+            onClick={() => guessLetters(el)} key={el} id={el}>{el.toUpperCase()}</button>
+          )}
+        )}
       </section>
+      <button className="new-game">New Game</button>
     </>
   )
 }
